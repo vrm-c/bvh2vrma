98f9c27d1a5eb5e77b027d5997a283b62941c198
0b5vr <0b5vr@0b5vr.com>
2023-07-07 17:44:57 +0900

    improve: Improve bone estimation

---
+++ src/lib/bvh-converter/pickByProbability.ts
@@ -0,0 +1,46 @@
+/**
+ * Pick an item from the given array by probability evaluation functions.
+ */
+export function pickByProbability<T>(
+  array: T[],
+  evaluators: {
+    func: ( value: T ) => number,
+    weight: number,
+  }[],
+): T {
+  const results = array.map(() => 0.0);
+
+  // execute each evaluators
+  for (const evaluator of evaluators) {
+    const { func, weight } = evaluator;
+    let min = Infinity;
+    let max = -Infinity;
+
+    const evaluatorResults = array.map((value) => {
+      const evaluatorResult = func(value);
+      min = Math.min(min, evaluatorResult);
+      max = Math.max(max, evaluatorResult);
+      return evaluatorResult;
+    } );
+
+    const range = max - min;
+    if (range > 0.0) {
+      evaluatorResults.map((v, i) => {
+        results[i] += weight * (v - min) / range;
+      });
+    }
+  }
+
+  // find an index of an element which has the highest result
+  let highestResult = -Infinity;
+  let highestIndex = 0;
+
+  for (const [ i, result ] of results.entries()) {
+    if (result > highestResult) {
+      highestResult = result;
+      highestIndex = i;
+    }
+  }
+
+  return array[highestIndex];
+}
